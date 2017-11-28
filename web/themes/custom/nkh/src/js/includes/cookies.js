62de68ab31c28b39c238988984b3db6d4568fb43
Alexis Findiesen <alexis.findiesen@agencychief.com>
2017-11-28 13:10:53 -0500

    Commented out the cookie js because it was breaking the theme

--- web/themes/custom/nkh/src/js/includes/cookies.js
+++ web/themes/custom/nkh/src/js/includes/cookies.js
@@ -1,29 +1,29 @@
-var Cookies = require('js-cookie');
-
-// JS cookies https://github.com/js-cookie/js-cookie
-(function ($) {
-  // DOC READY
-
-
-            var $ctaCloseButton = $("button.toggle-close");
-
-            //On click to toggle settings
-            $ctaCloseButton.click(function() {
-                $(".entity--cta--alert").slideToggle('slow');
-
-                $.cookie('is-hidden', 'hidden', { path: '/' }); // set cookie
-
-                return false;
-                console.log($.cookie('is-hidden') + ' original click');
-            });
-
-
-
-            if($.cookie('is-hidden') === null || $.cookie('is-hidden') === "" || $.cookie('is-hidden') === "null" || $.cookie('is-hidden') === undefined){
-                // $(".entity--cta--alert").removeClass('hide');
-                $.cookie('is-hidden', null); // remove cookie
-            }else{
-               // $(".entity--cta--alert" ).hide();
-            }
-
-})(jQuery);
+// var Cookies = require('js-cookie');
+//
+// // JS cookies https://github.com/js-cookie/js-cookie
+// (function ($) {
+//   // DOC READY
+//
+//
+//             var $ctaCloseButton = $("button.toggle-close");
+//
+//             //On click to toggle settings
+//             $ctaCloseButton.click(function() {
+//                 $(".entity--cta--alert").slideToggle('slow');
+//
+//                 $.cookie('is-hidden', 'hidden', { path: '/' }); // set cookie
+//
+//                 return false;
+//                 console.log($.cookie('is-hidden') + ' original click');
+//             });
+//
+//
+//
+//             if($.cookie('is-hidden') === null || $.cookie('is-hidden') === "" || $.cookie('is-hidden') === "null" || $.cookie('is-hidden') === undefined){
+//                 // $(".entity--cta--alert").removeClass('hide');
+//                 $.cookie('is-hidden', null); // remove cookie
+//             }else{
+//                // $(".entity--cta--alert" ).hide();
+//             }
+//
+// })(jQuery);
