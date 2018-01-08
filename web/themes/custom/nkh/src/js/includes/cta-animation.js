b5df9dab6c6462d684dfa7fdf2a22284477d33c1
Charles Hwang <charles.hwang@agencychief.com>
2018-01-08 14:21:49 -0500

    NKH-827: Wrap CTA animation script in a Drupal behavior

--- web/themes/custom/nkh/src/js/includes/cta-animation.js
+++ web/themes/custom/nkh/src/js/includes/cta-animation.js
@@ -1,27 +1,29 @@
-// Append and restart cta animation on mouseenter
-(function ($) {
-  // DOC READY
-  $(function () {
+(function ($, Drupal) {
+    Drupal.behaviors.ctaBehavior = {
+        attach: function(context, settings) {
 
-    $('#block-views-block-cta-references-basic-page').mouseenter(function() {
-      var $div = $(this);
+            // Append and restart cta animation on mouseenter
+            $('#block-views-block-cta-references-basic-page').mouseenter(function() {
+                var $div = $(this);
 
-      var img = document.createElement('img');
-      img.src = "/themes/custom/nkh/images/btn-bingo-loop.gif?" + new Date().getTime();
+                var img = document.createElement('img');
+                img.src = "/themes/custom/nkh/images/btn-bingo-loop.gif?" + new Date().getTime();
 
-      $div.append('<div class="cta-animation"></div>');
+                $div.append('<div class="cta-animation"></div>');
 
-      $(img).on('load', function(){
-        $('.cta-animation').css({backgroundImage: "url("+img.src+")"});
-      });
-    });
+                $(img).on('load', function(){
+                  $('.cta-animation').css({backgroundImage: "url("+img.src+")"});
+                });
+            });
+            
+            $('#block-views-block-cta-references-basic-page').mouseleave(function() {
 
-    $('#block-views-block-cta-references-basic-page').mouseleave(function() {
-      var animation = $(this).find('.cta-animation');
-      if (animation) {
-        animation.remove();
-      }
-    });
+                var animation = $(this).find('.cta-animation');
 
-  });
-})(jQuery);
+                if (animation) {
+                  animation.remove();
+                }
+            });
+        }
+    };
+})(jQuery, Drupal);
