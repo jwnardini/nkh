e7e1d159a7f1a3b621a271477aaea6cf7b4d3fb9
Alexis Findiesen <alexis.findiesen@agencychief.com>
2018-01-10 16:44:40 -0500

    Search theming

--- web/themes/custom/nkh/src/js/includes/accordion.js
+++ web/themes/custom/nkh/src/js/includes/accordion.js
@@ -1,24 +1,38 @@
-// Accordion styling functionality and animation
-(function ($) {
-  // DOC READY
-  $(function () {
+(function ($, Drupal) {
+  Drupal.behaviors.resourceFilters = {
+    attach: function(context, settings) {
     
-    var accordioncontainer = '.custom-accordion',
-      accordioncontent = '.accordion-content',
-      accordiontrigger = '.accordion-trigger';
+      // var accordioncontainer = '.custom-accordion',
+      //   accordioncontent = '.accordion-content',
+      //   accordiontrigger = '.accordion-trigger';
 
-    $(accordioncontainer).each(function (i) {
-      $(this).addClass('tm-accordion-enabled');
-      if (i > 0) {
-        $(this).addClass('collapsed').find(accordioncontent).slideUp('fast');
-      }
-    });
+      // $(accordioncontainer).each(function (i) {
+      //   $(this).addClass('tm-accordion-enabled');
+      //   if (i > 0) {
+      //     $(this).addClass('collapsed').find(accordioncontent).slideUp('fast');
+      //   }
+      // });
 
-    $(accordioncontainer + ' ' + accordiontrigger + ' a').click(function (e) {
-      e.preventDefault();
-      $(this).closest(accordioncontainer).toggleClass('collapsed');
-      $(this).parent().next(accordioncontent).slideToggle('fast');
-    });
+      // $(accordioncontainer + ' ' + accordiontrigger + ' a').click(function (e) {
+      //   e.preventDefault();
+      //   $(this).closest(accordioncontainer).toggleClass('collapsed');
+      //   $(this).parent().next(accordioncontent).slideToggle('fast');
+      // });
 
-  });
-})(jQuery);
+      function resourceFiltersToggle() {
+          //only for the resource center
+          $('#views-exposed-form-resource-view-page-1 legend').on('click', function(e) {
+            //mobile only
+            if ($(window).width() < 1024) {
+              $(this).closest('fieldset.form-wrapper').toggleClass('collapsed');
+              $(this).siblings('div.fieldset-wrapper').toggleClass('mobile-hide');
+            };
+          });
+      };
+
+
+      resourceFiltersToggle();
+    }
+
+  };
+})(jQuery, Drupal);
