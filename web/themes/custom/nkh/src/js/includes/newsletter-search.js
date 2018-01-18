4919c3c2dbe2ac3cf7de21a2a80a9a7108f09c5e
Boris Kan <boris.kan@Boris-Kans-MacBook-Pro.local>
2018-01-18 13:35:58 -0500

    animation and styling match to the new search

--- web/themes/custom/nkh/src/js/includes/newsletter-search.js
+++ web/themes/custom/nkh/src/js/includes/newsletter-search.js
@@ -8,15 +8,15 @@
 
             //if (context == document) {
 
-                $( '.block-search' ).prepend( '<button class="header-search-form__toggle"><span class="element-invisible">Open the Search Form</span></button>');
-                $( '.block-search form' ).prepend( '<a class="icon icon-close"><span class="element-invisible">Close the search</span></a>');
+                $( '.block-exposedformsite-db-searchpage-form' ).prepend( '<button class="header-search-form__toggle"><span class="element-invisible">Open the Search Form</span></button>');
+                $( '.block-exposedformsite-db-searchpage-form form' ).prepend( '<a class="icon icon-close"><span class="element-invisible">Close the search</span></a>');
                 $( 'form.newsletter-signup' ).prepend( '<a class="icon icon-close"><span class="element-invisible">Close the search</span></a>');
 
                 var $everything = $(window);
                 // add class to this list for toggle class on the same page. Use this on footer/header sectionss.
-                var $blocks = $('.block-mobilecommonssignup .text-formatted, .block-newslettersignupform, .block-newslettersignupform-2, .block-search');
-                var $blocksButton = $('.block-mobilecommonssignup .mobile-alert-button, .block-newslettersignupform .text-formatted .header-newsletter-form__toggle, .block-newslettersignupform-2 .text-formatted .header-newsletter-form__toggle, .block-search .header-search-form__toggle');
-                var $blocksCloseButton = $('.block-search .icon-close, .block-newslettersignupform .icon-close, .block-newslettersignupform-2 .icon-close');
+                var $blocks = $('.block-mobilecommonssignup .text-formatted, .block-newslettersignupform, .block-newslettersignupform-2, .block-exposedformsite-db-searchpage-form');
+                var $blocksButton = $('.block-mobilecommonssignup .mobile-alert-button, .block-newslettersignupform .text-formatted .header-newsletter-form__toggle, .block-newslettersignupform-2 .text-formatted .header-newsletter-form__toggle, .block-exposedformsite-db-searchpage-form .header-search-form__toggle');
+                var $blocksCloseButton = $('.block-exposedformsite-db-searchpage-form .icon-close, .block-newslettersignupform .icon-close, .block-newslettersignupform-2 .icon-close');
 
                 // give the classes above a common share class.
                 $blocks.addClass('toggle-class');
